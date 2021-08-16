30a368a01a8795ed57ad01e8ba7e7cf7c108f3cd
andriann-rak <andrianina.rakoto2@gmail.com>
2021-08-16 03:55:25 +0300

    pull

--- middleware/validation/index.js
+++
@@ -1,9 +0,0 @@
-const {isAdmin} = require("./role")
-const {isValidStaff} = require("./staffValidation")
-
-module.exports = {
-    validation: [
-        isAdmin,
-        isValidStaff,
-    ]
-}
--- modules/auth/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router()
-const ctrl = require("./auth.controller")
-
-router.post("/", ctrl.login)
-router.get("/check-email/:email", ctrl.checkEmail)
-
-module.exports = router
